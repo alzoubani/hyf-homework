70e3ac571ae47c27734e3e9bc51843cf8fdca857
alzoubani <alzoubani@gmail.com>
2020-05-22 03:25:53 +0200

    Smart-ease (age-ify+ Goodboy-Oldboy + House pricey)

--- javascript/javascript1/week1/smart-ease.js
+++ javascript/javascript1/week1/smart-ease.js
@@ -1,3 +1,5 @@
+//smart ease
+
 let yearOfBirth =1984 ;
 let yearFuture =2025;
 let age=yearFuture - yearOfBirth;
@@ -18,5 +20,38 @@ if( shouldShowResultInDogYears == true){
 }else{
     dogYear=  dogYearFuture - dogYearOfBirth +15;
     console.log("Your dog will be 10 human years old in 2027")
+}
+
+//Housey pricey (A house price estimator)
+
+let huseHeight = 10 ;
+let huseDepth = 10 ;
+let husWidth = 8 ;
+let gardenSizeInM2 = 100 ;
+let volumeInMeters = husWidth * huseDepth * huseHeight;
+let requstedPrice =  2500000;
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+if(housePrice  == requstedPrice){
+    console.log( "Good price, as it is expected!")
+}
+ else if (housePrice < requstedPrice) {
+    console.log("Cheap prise")
+} else {
+    console.log("too much")
+}
+ huseHeight = 8 ; 
+huseDepth = 11 ;
+husWidth = 5 ;
+gardenSizeInM2 = 70 ;
+
+ requstedPrice =  1000000;
+
+if(housePrice  == requstedPrice){
+    console.log( "Good price, as it is expected!")
+}
+ else if (housePrice < requstedPrice) {
+    console.log("Cheap prise")
+} else {
+    console.log("too much")
 }
 
